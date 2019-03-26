ee723286119d15241f299a91b3eb8b39d08d52c6
Eyal Toledano <eyaltoledano@me.com>
2019-03-26 18:45:58 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,11 +3,18 @@ require 'nokogiri'
 # require libraries/modules here
 
 def create_project_hash
+  projects = {}
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css(".location-name").text,
+      percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+    }
+  end
+  projects
 end
-
-binding.pry
-
-kickstarter = create_project_hash
-# projects: kickstarter.css('li.project.grid_4')
+.
